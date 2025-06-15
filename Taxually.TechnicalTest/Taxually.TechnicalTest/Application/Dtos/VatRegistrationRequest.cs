0ee53cb31a9ca45b150bef4692f765861c6f9717
jvstDani92 <radomirovDaniel@gmail.com>
2025-06-15 20:32:52 +0200

    feat(domain): create request object

---
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Application/Dtos/VatRegistrationRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace Taxually.TechnicalTest.Application.Dtos
+{
+    public record VatRegistrationRequest
+    {
+        public string? CompanyName { get; init; }
+        public string? CompanyId { get; init; }
+        public string? Country { get; init; }
+
+        public VatRegistrationRequest() { }
+
+        public VatRegistrationRequest(string? companyName, string? companyId, string? country)
+        {
+            CompanyName = companyName;
+            CompanyId = companyId;
+            Country = country;
+        }
+    }
+
+}
